3d34d7ad4f07d3e4b13380115fe6a4e4ee23bebb
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2024-06-18 22:24:32 -0300

    improved style

--- js/cozinha/index.js
+++ js/cozinha/index.js
@@ -3,19 +3,19 @@ const END_FUNCIONAL = `https://cipaon.com.br/api/produto.php?token=${TOKEN}`;
 const URL = `https://cipaon.com.br/api/produto.php?`;
 var newResponse = '';
 
-    init();
+init();
 
-    function init(){
-        //criarProduto();
-        consultarApiProdutos();
-    }
-  
-    //------------------ LISTAR ------------------//
-    function orderByCategoria(a, b) {
-        return a - b;
-    }
+function init() {
+	//criarProduto();
+	consultarApiProdutos();
+}
 
-    /*function listarProdutos() {
+//------------------ LISTAR ------------------//
+function orderByCategoria(a, b) {
+	return a - b;
+}
+
+/*function listarProdutos() {
       ->FORMA DE FAZER COM AJAX<-
         console.log('entrei lista');
         $.ajax({
@@ -31,28 +31,26 @@ var newResponse = '';
             });
 
     }*/
-       
-    function consultarApiProdutos(){
-        $.getJSON(END_FUNCIONAL, function (response){
 
-            //categoria
-            newResponse = response.sort(orderByCategoria);
+function consultarApiProdutos() {
+	$.getJSON(END_FUNCIONAL, function (response) {
+		//categoria
+		newResponse = response.sort(orderByCategoria);
 
-            //testar caracter especial
-            newResponse.sort(function(a, b) {
-                return a.nome.localeCompare(b.nome);
-            });
+		//testar caracter especial
+		newResponse.sort(function (a, b) {
+			return a.nome.localeCompare(b.nome);
+		});
 
-            listarProdutos(newResponse);
+		listarProdutos(newResponse);
+	});
+}
 
-        });
-    }
+function listarProdutos(newResponse) {
+	var conteudoProduto = '';
 
-    function listarProdutos(newResponse) {
-        var conteudoProduto = '';
-        
-        newResponse.forEach(element => {
-            conteudoProduto += `<div class="ui card fluid" id="produto-${element.idProduto}">
+	newResponse.forEach((element) => {
+		conteudoProduto += `<div class="ui card fluid" id="produto-${element.idProduto}">
                                     <div class="ui top brown attached label">${element.nome}</div>
                                     <div class="blurring dimmable image">    
                                        <div class="ui green bottom right attached label">${element.idCategoria}</div>
@@ -71,28 +69,25 @@ var newResponse = '';
                                         </div>
                                     </div>
                                 </div>`;
-        });
-        $('#menu-produtos').append(conteudoProduto);
+	});
+	$('#menu-produtos').append(conteudoProduto);
+}
+//------------------ LISTAR ------------------//
 
-    }
-    //------------------ LISTAR ------------------//
+//------------------ EXCLUIR ------------------//
+$('#menu-produtos').on('click', '#delete', function () {
+	let item = $(this).closest('.ui.card').attr('id').split('-')[1];
 
-    //------------------ EXCLUIR ------------------//
-    $('#menu-produtos').on('click','#delete', function(){
-        let item = $(this).closest('.ui.card').attr('id').split('-')[1];
+	confirmarExclusao(item);
 
-        confirmarExclusao(item);
+	$('.ui.modal').modal('show');
+});
 
-        $('.ui.modal')
-            .modal('show')
-        ;
-    });
+function confirmarExclusao(item) {
+	$('#confirma-excluir-produtos').empty();
 
-    function confirmarExclusao(item){
-        $('#confirma-excluir-produtos').empty();
-
-        produto = newResponse.find(prod => prod.idProduto == item);
-        conteudo = `
+	produto = newResponse.find((prod) => prod.idProduto == item);
+	conteudo = `
         <div class="ui grid">
             <div class="four wide column">
                 <div class="ui medium image">
@@ -115,20 +110,24 @@ var newResponse = '';
             </div>
         </div>`;
 
-        $('#confirma-excluir-produtos').append(conteudo);
+	$('#confirma-excluir-produtos').append(conteudo);
 
-         //$('#confirma-excluir-produtos').off('click', '#deletar-produto').on('click', '#deletar-produto', function(){
-        $('#deletar-produto').off('click').on('click', function() {
-            excluirProduto(item);
-            $('.ui.modal').modal('hide');
-        });
+	//$('#confirma-excluir-produtos').off('click', '#deletar-produto').on('click', '#deletar-produto', function(){
+	$('#deletar-produto')
+		.off('click')
+		.on('click', function () {
+			excluirProduto(item);
+			$('.ui.modal').modal('hide');
+		});
 
-        $('.ui.black.deny.button').off('click').on('click', function() {
-            $('.ui.modal').modal('hide');
-        });
-    }
+	$('.ui.black.deny.button')
+		.off('click')
+		.on('click', function () {
+			$('.ui.modal').modal('hide');
+		});
+}
 
-    /*function excluirProduto(item){
+/*function excluirProduto(item){
         ->FORMA DE FAZER COM FETCH<-
        fetch(`https://cipaon.com.br/api/produto.php?token=${TOKEN}&idProduto=${item}`, {
             method: "DELETE",  
@@ -146,165 +145,162 @@ var newResponse = '';
         });
     }*/
 
-    function excluirProduto(item){
-        const DELETE = `https://cipaon.com.br/api/produto.php?token=${TOKEN}&idProduto=${item}`;
-        $.ajax({
-            url: DELETE,
-            type: 'DELETE',
-            success: function (response) {
-                console.log('produto deletado', item);
-                $(`#produto-${item}`).remove();
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        })
-    }
-    //------------------ EXCLUIR ------------------//
+function excluirProduto(item) {
+	const DELETE = `https://cipaon.com.br/api/produto.php?token=${TOKEN}&idProduto=${item}`;
+	$.ajax({
+		url: DELETE,
+		type: 'DELETE',
+		success: function (response) {
+			console.log('produto deletado', item);
+			$(`#produto-${item}`).remove();
+		},
+		error: function (error) {
+			console.log(error);
+		},
+	});
+}
+//------------------ EXCLUIR ------------------//
 
-    //------------------ PESQUISA ------------------//
-    $(document).on('click', '#btn-procurar-generica', function() {
-        $('#menu-produtos').empty();
-        pesquisarProdutoGenerico();
-       
-    });
+//------------------ PESQUISA ------------------//
+$(document).on('click', '#btn-procurar-generica', function () {
+	$('#menu-produtos').empty();
+	pesquisarProdutoGenerico();
+});
 
-    $(document).on('click', '#btn-limpar-generica', function() {
-        $('#menu-produtos').empty();
-        $('#pesquisa-generica').val('');
-        consultarApiProdutos();
-    });
+$(document).on('click', '#btn-limpar-generica', function () {
+	$('#menu-produtos').empty();
+	$('#pesquisa-generica').val('');
+	consultarApiProdutos();
+});
 
-    function pesquisarProdutoGenerico(){
-        let nomeProcurar = $('#pesquisa-generica').val();
-        if (nomeProcurar == '') {
-            alertaPesquisaGenericaVazia();
-            return;
-        } else {
-            const resultadoProcurar = newResponse.filter(json => nomeProcurar == json.nome);
-            listarProdutos(resultadoProcurar);
-        }
-    }
+function pesquisarProdutoGenerico() {
+	let nomeProcurar = $('#pesquisa-generica').val();
+	if (nomeProcurar == '') {
+		alertaPesquisaGenericaVazia();
+		return;
+	} else {
+		const resultadoProcurar = newResponse.filter(
+			(json) => nomeProcurar == json.nome
+		);
+		listarProdutos(resultadoProcurar);
+	}
+}
 
-    function alertaPesquisaGenericaVazia(){
-        $('#aviso-pesquisa-vazia').popup('show');
-    }
-    //------------------ PESQUISA ------------------//
-        
-    //------------------ CHAMAR EDIT ------------------//
-    $('#menu-produtos').on('click','#edite', function(){
-        console.log('entrei no edit');
-        let item = $(this).closest('.ui.card').attr('id').split('-')[1];
-        console.log(item);
+function alertaPesquisaGenericaVazia() {
+	$('#aviso-pesquisa-vazia').popup('show');
+}
+//------------------ PESQUISA ------------------//
 
-        produtoEditar = `<div class="ui card fluid" id="produto-${item.idProduto}">
+//------------------ CHAMAR EDIT ------------------//
+$('#menu-produtos').on('click', '#edite', function () {
+	console.log('entrei no edit');
+	let item = $(this).closest('.ui.card').attr('id').split('-')[1];
+	console.log(item);
+
+	produtoEditar = `<div class="ui card fluid" id="produto-${item.idProduto}">
                                 <div class="ui top brown attached label">${item.nome}</div>
                                 <div class="blurring dimmable image">    
                                    <div class="ui green bottom right attached label">${item.idCategoria}</div>
                                         <img src="${item.foto}">
                                 </div>
                             </div>`;
-        $('#editar-produto').append(produtoEditar);
+	$('#editar-produto').append(produtoEditar);
+});
+//------------------ CHAMAR EDIT ------------------//
 
+//------------------ PESQUISA DE VERDADE POOOO ------------------//
+$('#pesquisa-dinamica').on('input', function () {
+	pesquisarProduto();
+});
 
-    });
-    //------------------ CHAMAR EDIT ------------------//
-    
-   //------------------ PESQUISA DE VERDADE POOOO ------------------//
-    $('#pesquisa-dinamica').on('input', function(){
-        pesquisarProduto();
-    });
+function pesquisarProduto() {
+	$('#menu-produtos').empty();
 
-    function pesquisarProduto(){
-        $('#menu-produtos').empty();
+	let pesquisa = $('#pesquisa-dinamica').val();
+	pesquisa.toLowerCase();
 
-        let pesquisa = $('#pesquisa-dinamica').val();
-        pesquisa.toLowerCase();
-       
-        const resultado = newResponse.filter(json => json.nome.toLowerCase().startsWith(pesquisa));
+	const resultado = newResponse.filter((json) =>
+		json.nome.toLowerCase().startsWith(pesquisa)
+	);
 
-        $('#pesquisa-generica').val('');
-        listarProdutos(resultado);
-    }
-    //------------------ PESQUISA DE VERDADE POOOO ------------------//
+	$('#pesquisa-generica').val('');
+	listarProdutos(resultado);
+}
+//------------------ PESQUISA DE VERDADE POOOO ------------------//
 
-    //------------------ CRIAR DE VERDADE POOOO ------------------//
-    const inputs = $('#criar-produto').find('input[required]');
-    const button = $('#btn-criar-produto');
+//------------------ CRIAR DE VERDADE POOOO ------------------//
+const inputs = $('#criar-produto').find('input[required]');
+const button = $('#btn-criar-produto');
 
-    function checkInputs() {
-        let allFilled = true;
-        inputs.each(function() {
-            if ($(this).val() === '') {
-                allFilled = false;
-                return false;
-            }
-        });
-        button.prop('disabled', !allFilled);
-    }
+function checkInputs() {
+	let allFilled = true;
+	inputs.each(function () {
+		if ($(this).val() === '') {
+			allFilled = false;
+			return false;
+		}
+	});
+	button.prop('disabled', !allFilled);
+}
 
-    inputs.on('input', checkInputs);
-    checkInputs();
+inputs.on('input', checkInputs);
+checkInputs();
 
-    $('#criar-produto').on('submit', function(event){
-        event.preventDefault();
-        coletarDadosCreate($(this));
-    });
+$('#criar-produto').on('submit', function (event) {
+	event.preventDefault();
+	coletarDadosCreate($(this));
+});
 
-    function coletarDadosCreate(form){
-        const dados = form.serializeArray();
-        let dadosObj = {};
-        let fieldsErr = [];
+function coletarDadosCreate(form) {
+	const dados = form.serializeArray();
+	let dadosObj = {};
+	let fieldsErr = [];
 
-        dados.forEach(item => {
-            if (item.value == "" && item.name !== "foto") {
-                fieldsErr.push(item.name);
-            }
-                dadosObj[item.name] = item.value;
-        });
+	dados.forEach((item) => {
+		if (item.value == '' && item.name !== 'foto') {
+			fieldsErr.push(item.name);
+		}
+		dadosObj[item.name] = item.value;
+	});
 
-        if (fieldsErr.length !== 0) {
-                $('#erro-criacao-produtos').modal('show');
-                            
-            conteudo = `O campo ${fieldsErr.toString()} está vazio!`;
-            
-            $('#errModel').append(conteudo);
+	if (fieldsErr.length !== 0) {
+		$('#erro-criacao-produtos').modal('show');
 
-            return;
-        }
+		conteudo = `O campo ${fieldsErr.toString()} está vazio!`;
 
-        console.log(dadosObj);
-        criarProduto(dadosObj);
-    }
+		$('#errModel').append(conteudo);
 
-    function criarProduto(dadosObj) {
-        $.ajax({
-            url: URL,
-            method: "POST",
-            data: {
-                token: TOKEN,
-                nome: dadosObj.nome,
-                idCategoria: dadosObj.categoria,
-                foto: dadosObj.foto,
-                preco: dadosObj.preco,
-                descricao: dadosObj.descricao
-            },
-            success: function (data) {
-                $('#escolha')
-                    .modal('show')
-                ; 
+		return;
+	}
 
-                modalEscolhaContinuarVisualizar();
-                $('#criar-produto')[0].reset();
-            },
-            error: function (error) {
-            }
-        })
-    }
+	criarProduto(dadosObj);
+}
 
-    function modalEscolhaContinuarVisualizar(){
-        $('#escolha-produtos').empty();
-        conteudo = `
+function criarProduto(dadosObj) {
+	$.ajax({
+		url: URL,
+		method: 'POST',
+		data: {
+			token: TOKEN,
+			nome: dadosObj.nome,
+			idCategoria: dadosObj.categoria,
+			foto: dadosObj.foto,
+			preco: dadosObj.preco,
+			descricao: dadosObj.descricao,
+		},
+		success: function (data) {
+			$('#escolha').modal('show');
+
+			modalEscolhaContinuarVisualizar();
+			$('#criar-produto')[0].reset();
+		},
+		error: function (error) {},
+	});
+}
+
+function modalEscolhaContinuarVisualizar() {
+	$('#escolha-produtos').empty();
+	conteudo = `
                 <div class=""> 
                     <a href="/listar.html">
                         <button class="ui button" id="btn-visualizar">Visualizar Produto</button>
@@ -315,21 +311,16 @@ var newResponse = '';
                 </div>
         `;
 
-        $('#escolha-produtos').append(conteudo);       
-    }
-    //------------------ CRIAR DE VERDADE POOOO ------------------//
+	$('#escolha-produtos').append(conteudo);
+}
+//------------------ CRIAR DE VERDADE POOOO ------------------//
 
+// const formProduto = document.querySelector('#criar-produto')
 
- // const formProduto = document.querySelector('#criar-produto')
-        
-        // formProduto.addEventListener('submit', (event) => {
+// formProduto.addEventListener('submit', (event) => {
 
-        //     event.preventDefault()
+//     event.preventDefault()
 
-        //     const data = Object.fromEntries(new FormData(event.target).entries());
-        //     console.log(data);
-        // });
-
-      
-    
-
+//     const data = Object.fromEntries(new FormData(event.target).entries());
+//     console.log(data);
+// });
